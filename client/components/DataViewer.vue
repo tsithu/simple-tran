004f23dc410f3eb55e03e884c068e696e71b4216
Tony Si Thu <tsithu@gmail.com>
2020-11-16 15:58:58 +0630

    feat: :sparkles: added DataViewer component
    To display table data, to search by currency, by date range, by status. just a sample

--- client/components/DataViewer.vue
+++ client/components/DataViewer.vue
@@ -1,5 +1,118 @@
 <template>
-  <div>
-    <strong>this is dataviewer</strong>
+  <div class="my-3">
+    <form>
+      <div class="form-group">
+        <label for="filter">Search</label>
+        <div>
+          <input id="filter" v-model="filter" type="text" class="form-control">
+        </div>
+      </div>
+      <div>
+        <button class="btn btn-info" @click.prevent="loadData('byCurrency')">
+          By Currency
+        </button>
+        <button class="btn btn-info" @click.prevent="loadData('byDateRange')">
+          By Date Range
+        </button>
+        <button class="btn btn-info" @click.prevent="loadData('byStatus')">
+          By Status
+        </button>
+      </div>
+    </form>
+    <div class="my-4">
+      <button class="btn btn-primary" @click.prevent="loadData()">
+        Reload
+      </button>
+    </div>
+    <table class="table">
+      <thead>
+        <th v-for="col in columns" :key="col">
+          {{ col }}
+        </th>
+      </thead>
+      <tbody>
+        <tr v-for="(record, index) in records" :key="`${record.id}-${record.code}`">
+          <td v-for="field in fields" :key="field.key">
+            {{ displayValue(record, index, field.key) }}
+          </td>
+        </tr>
+      </tbody>
+      <tfoot>
+        <td :colspan="fields.length">
+          <strong>Total Records</strong>
+          <span class="badge badge-info mr-2">{{ totalRecords }}</span>
+        </td>
+      </tfoot>
+    </table>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      filter: '',
+      records: [],
+      totalRecords: 0,
+      fields: [
+        {
+          key: 'index',
+          label: 'No.'
+        },
+        {
+          key: 'code',
+          label: 'Id'
+        },
+        {
+          key: 'payment',
+          label: 'Payment'
+        },
+        {
+          key: 'transactionDate',
+          label: 'Transaction Date'
+        },
+        {
+          key: 'status',
+          label: 'Status'
+        }
+      ]
+    }
+  },
+  computed: {
+    columns() {
+      return this.fields.map(f => f.label)
+    }
+  },
+  created() {
+    this.loadData()
+  },
+  methods: {
+    async loadData(filterType) {
+      const { restEndPoint } = this.$config
+      const api = `${restEndPoint}/transactions`
+      const dates = this.filter.split(',')
+      const mapping = {
+        byCurrency: `${api}/currency/${this.filter}`,
+        byDateRange: `${api}/transaction-date/${dates[0]}/${dates[1]}`,
+        byStatus: `${api}/status/${this.filter}`
+      }
+
+      const { data } = await this.$http.get(filterType ? mapping[filterType.toString()] : api)
+      const { results, total } = data
+
+      this.records = results
+      this.totalRecords = total
+    },
+    displayValue(record, index, key) {
+      switch (key) {
+        case 'index':
+          return `${index + 1}.`
+        case 'payment':
+          return `${record.amount} ${record.currencyCode}`
+        default:
+          return record[key.toString()]
+      }
+    }
+  }
+}
+</script>
